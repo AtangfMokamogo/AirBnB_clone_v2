68ba4c89beb62882f48a517276fe9e492616c374
AtangFMokamogo <atangfmokamogo@outlook.com>
2023-04-18 17:54:21 +0200

    corrected typo on script setup_mysql_dev.sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -11,7 +11,6 @@ GRANT ALL PRIVILEGES
     ON `hbnb_dev_db`.*
     TO'hbnb_dev'@'localhost'
     IDENTIFIED BY 'hbnb_dev_pwd';
-
 GRANT SELECT PRIVILEGES
     ON `performance_schema`.*
     TO 'hbnb_dev'@'localhost'
