43183620394382415334d29338ac39de2f56753b
AtangFMokamogo <atangfmokamogo@outlook.com>
2023-04-18 18:06:34 +0200

    corrected typo on script setup_mysql_dev.sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,18 +1,14 @@
--- Creating a MySQL server database hbnb_dev_db:
--- Grant SELECT priviledge to user on db performance_schema.
--- Granting user hbnb_dev privileges on database
--- Creating a new user in localhost with password hbnb_dev_pwd
-
+-- Create a new database hbnb_dev_db
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER
-    IF NOT EXISTS 'hbnb_dev'@'localhost'
-    IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES
-    ON `hbnb_dev_db`.*
-    TO'hbnb_dev'@'localhost'
-    IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT SELECT
-    ON `performance_schema`.*
-    TO 'hbnb_dev'@'localhost'
-    IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- Create a new user hbnb_dev in localhost
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- Grant all privileges on hbnb_dev_db to user hbnb_dev
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
+-- Grant select privilege on performance_schema to user hbnb_dev
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
+-- Flush privileges to apply changes
 FLUSH PRIVILEGES;
