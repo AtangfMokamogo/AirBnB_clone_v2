e638eba4b03da2097b67e7840deedd803c555918
AtangFMokamogo <atangfmokamogo@outlook.com>
2023-04-18 18:17:22 +0200

    corrected typo on script setup_mysql_test.sql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,18 +1,18 @@
 -- Creating a MySQL server database hbnb_test_db:
--- Grant SELECT priviledge to user on db performance_schema.
--- Grant user hbnb_test all privileges on hbnb_test_db
--- Creating a new user in localhost with password hbnb_test_pwd
-
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+-- Creating a new user in localhost with password hbnb_test_pwd
 CREATE USER
     IF NOT EXISTS 'hbnb_test'@'localhost'
     IDENTIFIED BY 'hbnb_test_pwd';
+
+-- Grant user hbnb_test all privileges on hbnb_test_db
 GRANT ALL PRIVILEGES
     ON `hbnb_test_db`.*
     TO'hbnb_test'@'localhost'
-    IDENTIFIED BY 'hbnb_test_pwd';
+
+-- Grant SELECT priviledge to user on db performance_schema.
 GRANT SELECT
     ON `performance_schema`.*
     TO 'hbnb_test'@'localhost'
-    IDENTIFIED BY 'hbnb_test_pwd';
 FLUSH PRIVILEGES;
