17752c81dbd32198ed805add0132da53a6a2c6c1
AtangFMokamogo <atangfmokamogo@outlook.com>
2023-04-18 18:21:52 +0200

    corrected typo on script setup_mysql_test.sql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -6,13 +6,13 @@ CREATE USER
     IF NOT EXISTS 'hbnb_test'@'localhost'
     IDENTIFIED BY 'hbnb_test_pwd';
 
--- Grant user hbnb_test all privileges on hbnb_test_db
+-- Granting user hbnb_test privileges on database
 GRANT ALL PRIVILEGES
     ON `hbnb_test_db`.*
-    TO'hbnb_test'@'localhost'
+    TO'hbnb_test'@'localhost';
 
 -- Grant SELECT priviledge to user on db performance_schema.
 GRANT SELECT
     ON `performance_schema`.*
-    TO 'hbnb_test'@'localhost'
+    TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
