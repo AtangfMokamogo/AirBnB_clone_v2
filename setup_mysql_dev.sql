a08a762a62bdb197dcbce5731b8dfadd7fdd96db
AtangFMokamogo <atangfmokamogo@outlook.com>
2023-04-18 17:48:25 +0200

    corrected checker error on script setup_mysql_dev.sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -5,7 +5,7 @@
 
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER
-    IF NO EXISTS 'hbnb_dev'@'localhost'
+    IF NOT EXISTS 'hbnb_dev'@'localhost'
     IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES
     ON `hbnb_dev_db`.*
