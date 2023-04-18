fe900b2c6ef2935f808fb7fa1420b29603fc7a3f
AtangFMokamogo <atangfmokamogo@outlook.com>
2023-04-18 17:44:15 +0200

    corrected checker error on script setup_mysql_dev.sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,12 +1,19 @@
 -- Creating a MySQL server database hbnb_dev_db:
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-
--- Creating a new user in localhost with password hbnb_dev_pwd
-CREATE USER IF NO EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-
--- Granting user hbnb_dev privileges on database
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-
 -- Grant SELECT priviledge to user on db performance_schema.
-GRANT SELECT PRIVILEGES ON performance_schema.* TO 'hbnb_dev'@'localhost';
+-- Granting user hbnb_dev privileges on database
+-- Creating a new user in localhost with password hbnb_dev_pwd
+
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER
+    IF NO EXISTS 'hbnb_dev'@'localhost'
+    IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES
+    ON `hbnb_dev_db`.* TO
+    'hbnb_dev'@'localhost'
+    IDENTIFIED BY 'hbnb_dev_pwd';
+
+GRANT SELECT PRIVILEGES
+    ON `performance_schema`.*
+    TO 'hbnb_dev'@'localhost'
+    IDENTIFIED BY 'hbnb_dev_pwd';;
 FLUSH PRIVILEGES;
